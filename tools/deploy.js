3ba1a6ca08fdabb835033545f762296141af675e
Takuya Matsuyama <nora@odoruinu.net>
2016-04-16 17:21:35 +0900

    change deploy settings

--- tools/deploy.js
+++ tools/deploy.js
@@ -10,7 +10,7 @@ import task from './lib/task';
 // TODO: Update deployment URL
 const remote = {
   name: 'github',
-  url: 'https://github.com/{user}/{repo}.git',
+  url: 'https://github.com/noradaiko/noradaiko.github.io.git',
   branch: 'gh-pages',
 };
 
