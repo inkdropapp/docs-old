46a1aff37329ac883eb11ac00331f7328d993dfc
Takuya Matsuyama <nora@odoruinu.net>
2017-06-13 17:35:03 +0900

    Add regular menus

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,16 +1,10 @@
-/**
- * React Static Boilerplate
- * https://github.com/koistya/react-static-boilerplate
- * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
- */
-
 import React from 'react'
 import './Navigation.scss'
 import Link from '../Link'
 
 function Navigation () {
   return (
-    <div className='ui grid'>
+    <nav className='ui grid'>
       <div className='row'>
         <a href='https://www.inkdrop.info/' className='app--logo'>
           <img
@@ -24,7 +18,31 @@ function Navigation () {
 
         <ul className='app--navbar reset-list un-select'>
           <li>
-            <Link to='/'>Docs</Link>
+            <a href='https://www.inkdrop.info/features'>
+              Features
+            </a>
+          </li>
+          <li>
+            <Link to='/'>
+              Docs
+            </Link>
+          </li>
+          <li>
+            <a href='https://www.inkdrop.info/plugins'>
+              Plugins
+            </a>
+          </li>
+          <li className='ui simple dropdown item'>
+            More
+            <i className='dropdown icon' />
+            <div className='menu'>
+              <a className='item' href='https://www.inkdrop.info/demo'>Demo</a>
+              <a className='item' href='https://www.inkdrop.info/pricing'>Pricing</a>
+              <Link className='item' to='/faq'>FAQ</Link>
+              <div className='divider' />
+              <a className='item' href='https://twitter.com/inkdrop_app'>Twitter</a>
+              <a className='item' href='https://medium.com/@inkdrop'>Blog</a>
+            </div>
           </li>
           <li>
             <a href='https://www.inkdrop.info/login' className='login'>
@@ -35,7 +53,7 @@ function Navigation () {
         </ul>
 
       </div>
-    </div>
+    </nav>
   )
 }
 
