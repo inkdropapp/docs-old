6a6c1ed3e0721d9a57a27105e6efd2a56d4ceb46
Takuya Matsuyama <hi@craftz.dog>
2020-09-24 16:33:54 +0900

    fix(release-note): add missing release note menu

--- src/components/info-menu.js
+++ src/components/info-menu.js
@@ -13,24 +13,34 @@ export default class InfoMenu extends Component {
     return (
       <StaticQuery
         query={menuQuery}
-        render={data => (
-          <Menu className="manual-menu">
-            <h3 className="ui header">
-              <i className="info icon" />
-              <div className="content">About</div>
-            </h3>
-            <ul className="article-list">
-              {data.allMarkdownRemark.edges.map(edge => {
-                const { path, title } = edge.node.frontmatter
-                return (
-                  <li key={path}>
-                    <MenuLink to={path}>{title}</MenuLink>
-                  </li>
-                )
-              })}
-            </ul>
-          </Menu>
-        )}
+        render={data => {
+          const pages = [
+            ...data.allMarkdownRemark.edges.map(edge => ({
+              path: edge.node.frontmatter.path,
+              title: edge.node.frontmatter.title
+            })),
+            { path: '/releases', title: 'Release Notes' }
+          ].sort((i1, i2) => i1.title.localeCompare(i2.title))
+
+          return (
+            <Menu className="manual-menu">
+              <h3 className="ui header">
+                <i className="info icon" />
+                <div className="content">About</div>
+              </h3>
+              <ul className="article-list">
+                {pages.map(page => {
+                  const { path, title } = page
+                  return (
+                    <li key={path}>
+                      <MenuLink to={path}>{title}</MenuLink>
+                    </li>
+                  )
+                })}
+              </ul>
+            </Menu>
+          )
+        }}
       />
     )
   }
